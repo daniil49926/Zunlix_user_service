444e3dc6b9bd83f82cffee2f54ee5fd16748636f
kaMata0 <daniil49926@gmail.com>
2023-09-01 16:08:23 +0300

    added requirements file and load environ function

---
+++ src/core/utils/load_env.py
@@ -0,0 +1,12 @@
+def load_environ(base_dir: str) -> None:
+    """
+    The environment variables file must be on the same level as the base directory of the application
+    :param base_dir: string value of the base directory of the application
+    :type base_dir: str
+    """
+    import os
+
+    from dotenv import load_dotenv
+
+    if os.path.exists(os.path.join(os.path.dirname(base_dir), ".env")):
+        load_dotenv(os.path.join(os.path.dirname(base_dir), ".env"))
